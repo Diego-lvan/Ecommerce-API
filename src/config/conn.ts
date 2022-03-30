e4d31b66fde21baf5a472916826d4c3eb2d3a726
Diego Iván <dcorreanavarrete@gmail.com>
2022-03-29 20:07:01 -0600

    fixs

--- src/config/conn.ts
+++ src/config/conn.ts
@@ -1,7 +1,8 @@
 import mysql from "mysql2/promise";
+import "dotenv/config";
 
 const pool = mysql.createPool({
-  host: process.env.dbHost,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PWD,
