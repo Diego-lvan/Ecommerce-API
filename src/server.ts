dfae600045045b18c5a53afd35f78f8dd56e66d9
Diego Iván <dcorreanavarrete@gmail.com>
2022-03-29 21:00:27 -0600

    changing folder structure

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import "dotenv/config";
 import cors from "cors";
 import { authRouter, productsRouter } from "./routes";
 import "./config/conn";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT! || 5000;
 
 app.use(cors());
 app.use(express.json());
+const config = { extensions: ["png", "jpg", "svg", "jpeg"] };
+app.use(express.static(path.join("upload"), config));
 
 app.use(authRouter, productsRouter);
 
