aece97be9b025d7980cbcb85a9514489db084889
Diego <dcorreanavarrete@gmail.com>
2022-04-15 15:10:34 -0500

    installing supertest

---
+++ src/controllers/auth.test.ts
@@ -0,0 +1,7 @@
+describe("GET auth", () => {
+  test("Testing auth", () => {
+    const expected = "hola mundo";
+    const result = "hola mundo";
+    expect(result).toStrictEqual(expected);
+  });
+});
