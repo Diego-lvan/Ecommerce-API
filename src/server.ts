f4b8520c8bd03c2a0ad772a7d07f8c619328dbca
Diego <dcorreanavarrete@gmail.com>
2022-04-17 18:20:48 -0500

    exporting server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 const PORT = process.env.PORT! || 5000;
 
-app.listen(PORT || 5000, () => {
+const server = app.listen(PORT || 5000, () => {
   console.log(`listening in port ${PORT || 5000}`);
 });
+
+export default server;
