1b10fb9e6d418f234b95be17356ea55809d41d34
Diego <dcorreanavarrete@gmail.com>
2022-04-17 18:33:02 -0500

    deleting coverage folder

--- src/controllers/auth.test.ts
+++
@@ -1,7 +0,0 @@
-describe("GET auth", () => {
-  test("Testing auth", () => {
-    const expected = "hola mundo";
-    const result = "hola mundo";
-    expect(result).toStrictEqual(expected);
-  });
-});
