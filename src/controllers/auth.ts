11c1a6d21ece9b851d630aae372816c55a3dec7b
Diego <dcorreanavarrete@gmail.com>
2022-04-17 21:31:48 -0500

    adding 'Bearer' to the token and handling errors with try catch

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import pool from "../config/conn";
 import "dotenv";
@@ -8,18 +8,26 @@ export interface User {
   email: string;
 }
 
-const loginUser = (req: Request, res: Response) => {
+const loginUser = (req: Request, res: Response, next: NextFunction) => {
   const userID: string = req.exists.userID;
-  const accessToken = jwt.sign(userID, process.env.ACCESS_TOKEN_SECRET!);
-  res.json({ accessToken });
+  try {
+    const accessToken = jwt.sign(userID, process.env.ACCESS_TOKEN_SECRET!);
+    res.json({ accessToken: `Bearer ${accessToken}` });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const registerUser = async (req: Request, res: Response) => {
+const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   if (req.exists !== undefined) return res.json({ msg: "user exists" });
   const user: User = req.body;
-  user.pwd = await bcrypt.hash(user.pwd, 10);
-  await pool.query("INSERT INTO user (email,pwd) VALUES (?,?)", [user.email, user.pwd]);
-  res.json({ email: user.email });
+  try {
+    user.pwd = await bcrypt.hash(user.pwd, 10);
+    await pool.query("INSERT INTO user (email,pwd) VALUES (?,?)", [user.email, user.pwd]);
+    res.status(201).json({ email: user.email });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { loginUser, registerUser };
