f7359aa7bc3ca0ef3b51e4b4935c8a563aee45d1
Diego <dcorreanavarrete@gmail.com>
2022-04-21 11:28:50 -0500

    add try catch block

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -10,50 +10,51 @@ interface JwtPayload {
 }
 
 const JWT_SECRET: string = process.env.ACCESS_TOKEN_SECRET;
-const getUserByID = async (req: Request, res: Response) => {
+const getUserByID = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userID: number = parseInt(req.params.userID);
     const [[data]]: any = await pool.query("SELECT userID,email,isAdmin FROM user WHERE userID = ?", [userID]);
+    if (!data) return res.status(404).json({ error: "not found" });
     res.json({ user: data });
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
 
-const getCurrentUser = async (req: Request, res: Response) => {
+const getCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userID: number = req.userID;
     const [[data]]: any = await pool.query("SELECT userID,email,isAdmin FROM user WHERE userID = ?", [userID]);
     res.json({ user: data });
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
 
-const updateUser = async (req: Request, res: Response) => {
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userID: number = req.userID;
     const newEmail: string = req.body.newEmail;
     await pool.query("UPDATE user SET email = ? WHERE userID = ?", [newEmail, userID]);
-    res.json({ success: true });
+    res.sendStatus(204);
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
 
-const updatePwd = async (req: Request, res: Response) => {
+const updatePwd = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userID = req.userID;
     const newPwd = req.body.newPwd;
     const oldPwd = req.body.oldPwd;
     const [[{ pwd }]]: any = await pool.query("SELECT pwd FROM user WHERE userID = ?", [userID]);
     const matches = await bcrypt.compare(oldPwd, pwd);
-    if (!matches) return res.json({ success: false, msg: "wrong pwd" });
+    if (!matches) return res.status(401).json({ msg: "wrong pwd" });
     const hashedNewPwd = await bcrypt.hash(newPwd, 10);
     await pool.query("UPDATE user set pwd = ? WHERE userID = ?", [hashedNewPwd, userID]);
-    res.json({ success: true });
+    res.sendStatus(204);
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
 
@@ -87,7 +88,7 @@ const forgotPassword = async (req: Request, res: Response) => {
   });
 };
 
-const resetPassword = async (req: Request, res: Response) => {
+const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
   const userID: number = parseInt(req.body.userID);
   const token: string = req.body.token;
   const newPwd: string = req.body.newPwd;
@@ -101,9 +102,9 @@ const resetPassword = async (req: Request, res: Response) => {
     if (userID !== payload.userID) return res.json({ error: "Wrong user id" });
     const hashedNewPwd = await bcrypt.hash(newPwd, 10);
     await pool.query("UPDATE user SET pwd = ? WHERE userID = ?", [hashedNewPwd, userID]);
-    res.json(req.body);
+    res.sendStatus(204);
   } catch (error) {
-    res.json({ error });
+    next(error);
   }
 };
 export { getUserByID, getCurrentUser, updateUser, updatePwd, forgotPassword, resetPassword };
